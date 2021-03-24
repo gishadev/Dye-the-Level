7e6a08f88b7f995675fda35eabf148d90fe5211a
GishaTheDeveloper <spaceshhz@gmail.com>
2021-03-24 16:00:04 +0200

    update scene and add rotate camera controller

---
+++ Dye-the-Level/Assets/_Project/Scripts/RotateCameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gisha.DyeTheWorld
+{
+    public class RotateCameraController : MonoBehaviour
+    {
+        [SerializeField] private float rotateSpeed;
+        [SerializeField] private float rotateAcceleration;
+
+        float _currentSpeed;
+
+        private void FixedUpdate()
+        {
+            var h = Input.GetAxis("Horizontal");
+            Rotate(h);
+        }
+
+        private void Rotate(float horizontal)
+        {
+            _currentSpeed = Mathf.Lerp(_currentSpeed, rotateSpeed * Mathf.Abs(horizontal), rotateAcceleration * Time.deltaTime);
+            transform.Rotate(-Vector3.up * horizontal, _currentSpeed * Time.deltaTime);
+        }
+    }
+}
